1f1bad4f82e5c829a0147e71dad73b41e5c49721
i-am-shunsuke <200557414@student.georgianc.on.ca>
2024-11-20 01:46:47 -0500

    Get succeeded for button function for start and score

--- app/src/main/java/com/example/myapplication02/MainActivity.java
+++ app/src/main/java/com/example/myapplication02/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication02;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView title;
 
     //the youtube about tutorial from 5:10
     @Override
@@ -21,5 +26,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        title=findViewById(R.id.title);
+
+//        Button start=findViewById(R.id.start);
+        ((Button)findViewById(R.id.start)).setOnClickListener(this);
+        ((Button)findViewById(R.id.score)).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        int id = view.getId(); // Get the ID of the clicked view
+        if (id == R.id.start) {
+            title.setText("Hello world");
+        } else if (id == R.id.score) {
+            title.setText("High Score Clicked");
+        }
     }
 }
